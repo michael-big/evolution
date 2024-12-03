041d6e0ab9df7f233adce689ae2def2154cf0429
Serg <64j@mail.ru>
2024-12-03 14:14:52 +0300

    Refactor ComponentsAutoloader

--- manager/src/ComponentsAutoloader.php
+++ manager/src/ComponentsAutoloader.php
@@ -3,52 +3,85 @@
 namespace EvolutionCMS\Manager;
 
 use Composer\ClassMapGenerator\ClassMapGenerator;
+use Exception;
 
 class ComponentsAutoloader
 {
-    private $paths = [];
+    /**
+     * @var string
+     */
+    protected string $path;
 
-    public function __construct($path)
+    /**
+     * @var array
+     */
+    private array $paths = [];
+
+    /**
+     * @param string $path
+     *
+     * @throws Exception
+     */
+    public function __construct(string $path)
     {
-        $path = $path;
-        if(is_dir($path)) {
+        if (is_dir($path)) {
             $this->path = $path;
         } else {
-            throw new \Exception("Path $path is not a directory");
+            throw new Exception("Path $path is not a directory");
         }
     }
 
+    /**
+     * @return void
+     * @throws Exception
+     */
     public function register()
     {
-        if(!file_exists($this->path . 'classmap.php') || !is_readable($this->path . 'classmap.php'))  {
+        if (!file_exists($this->path . 'classmap.php') || !is_readable($this->path . 'classmap.php')) {
             $this->build();
         }
+
         $classmap = require $this->path . 'classmap.php';
+
         spl_autoload_register(function ($class) use ($classmap) {
-            if(isset($classmap[$class])) {
+            if (isset($classmap[$class])) {
                 include $classmap[$class];
             }
         });
     }
 
-    public function addPath($path)
+    /**
+     * @param string $path
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function addPath(string $path)
     {
-        if(is_dir($path)) {
+        if (is_dir($path)) {
             $this->paths[] = $path;
         } else {
-            throw new \Exception("Path $path is not a directory");
+            throw new Exception("Path $path is not a directory");
         }
     }
 
+    /**
+     * @return void
+     * @throws Exception
+     */
     public function build()
     {
         $classmap = var_export($this->getClassmap(), true);
-        file_put_contents($this->path . 'classmap.php', "<php\nreturn {$classmap};\n");
+        file_put_contents($this->path . 'classmap.php', "<php\nreturn $classmap;\n");
     }
 
-    public function getClassmap()
+    /**
+     * @return array
+     * @throws Exception
+     */
+    public function getClassmap(): array
     {
-        if(!empty($this->paths)) {
+        if (!empty($this->paths)) {
             $generator = new ClassMapGenerator;
             foreach ($this->paths as $path) {
                 $generator->scanPaths($path);
@@ -56,7 +89,7 @@ class ComponentsAutoloader
 
             return $generator->getClassMap()->getMap();
         } else {
-            throw new \Exception("No paths to scan");
+            throw new Exception("No paths to scan");
         }
     }
 }
